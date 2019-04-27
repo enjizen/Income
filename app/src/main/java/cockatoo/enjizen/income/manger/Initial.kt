6ef7a7ebd1f3f3e75898799991ea8c23306df505
Wanchalerm Yuphasuk <enjizen.r@gmail.com>
2019-04-27 20:58:35 +0700

    Refactor code

--- app/src/main/java/cockatoo/enjizen/income/manger/Initial.kt
+++ app/src/main/java/cockatoo/enjizen/income/manger/Initial.kt
@@ -1,32 +1,22 @@
 package cockatoo.enjizen.income.manger
 
 import android.content.ContentValues
-import cockatoo.enjizen.income.R
-import cockatoo.enjizen.income.constant.BankInitialsConstant
 import cockatoo.enjizen.income.constant.DBContract
+import cockatoo.enjizen.income.extension.readJsonFile
 import cockatoo.enjizen.income.manger.db.DBHelper
 import cockatoo.enjizen.income.model.Bank
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 class Initial {
 
-    fun bank(){
-       val cursor = DBHelper.getInstance().getAll(DBContract.BankEntry.TABLE_NAME.value)
+    fun bank() {
+        val cursor = DBHelper.getInstance().getAll(DBContract.BankEntry.TABLE_NAME.value)
 
-        if(!cursor!!.moveToFirst()) {
-            val resource = Contextor.getInstance().context!!.resources
-            val bankList = ArrayList<Bank>()
-            bankList.add(Bank(1, resource.getString(R.string.bank_bbl), BankInitialsConstant.BBL.name, BankInitialsConstant.BBL.logo))
-            bankList.add(Bank(2, resource.getString(R.string.bank_kbank), BankInitialsConstant.KBANK.name, BankInitialsConstant.KBANK.logo))
-            bankList.add(Bank(3, resource.getString(R.string.bank_ktb), BankInitialsConstant.KTB.name, BankInitialsConstant.KTB.logo))
-            bankList.add(Bank(4, resource.getString(R.string.bank_tmb), BankInitialsConstant.TMB.name, BankInitialsConstant.TMB.logo))
-            bankList.add(Bank(5, resource.getString(R.string.bank_scb), BankInitialsConstant.SCB.name, BankInitialsConstant.SCB.logo))
-            bankList.add(Bank(6, resource.getString(R.string.bank_bay), BankInitialsConstant.BAY.name, BankInitialsConstant.BAY.logo))
-            bankList.add(Bank(7, resource.getString(R.string.bank_kkp), BankInitialsConstant.KKP.name, BankInitialsConstant.KKP.logo))
-            bankList.add(Bank(8, resource.getString(R.string.bank_cimb_thai), BankInitialsConstant.CIMBTHAI.name, BankInitialsConstant.CIMBTHAI.logo))
-            bankList.add(Bank(9, resource.getString(R.string.bank_tisco), BankInitialsConstant.TISCO.name, BankInitialsConstant.TISCO.logo))
-            bankList.add(Bank(10, resource.getString(R.string.bank_tbank), BankInitialsConstant.TBANK.name, BankInitialsConstant.TBANK.logo))
-            bankList.add(Bank(11, resource.getString(R.string.bank_uob), BankInitialsConstant.UOB.name, BankInitialsConstant.UOB.logo))
-            bankList.add(Bank(12, resource.getString(R.string.bank_tcd), BankInitialsConstant.TCD.name, BankInitialsConstant.TCD.logo))
+        if (!cursor!!.moveToFirst()) {
+            val listType = object : TypeToken<List<Bank>>() {}.type
+            val bankList = Gson().fromJson<List<Bank>>("bank.json".readJsonFile(), listType)
 
             val values = ArrayList<ContentValues>()
             bankList.forEach {
@@ -38,9 +28,10 @@ class Initial {
                 values.add(v)
             }
 
-        DBHelper.getInstance().insert(DBContract.BankEntry.TABLE_NAME.value, values)
+            DBHelper.getInstance().insert(DBContract.BankEntry.TABLE_NAME.value, values)
         }
 
+        cursor.close()
 
     }
 
